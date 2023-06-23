57bd24e010e591afc1eb4cc1806fa1111a59e0d2
Stephen Haase <shaase@gmail.com>
2023-06-23 10:31:18 -0600

    balancer fix

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.public_subnets[0]
   name    = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
