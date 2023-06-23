cf7e1c95f230b9c2545a5fe2ab16fb400defdd43
Stephen Haase <shaase@gmail.com>
2023-06-23 09:55:07 -0600

    error fix

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_s3_bucket_acl" "tf-course" {
 module "security-group" "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  # name    = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
