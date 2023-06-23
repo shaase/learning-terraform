85e74d0f7ff05f3380239b912181470526c7c494
Stephen Haase <shaase@gmail.com>
2023-06-23 14:01:05 -0600

    refactor for variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
-variable "instance_type" {
- description = "Type of EC2 instance to provision"
- default     = "t3.nano"
-}
+# secrets
 
 variable "AWS_ACCESS_KEY_ID" {
   type = string
@@ -9,4 +6,47 @@ variable "AWS_ACCESS_KEY_ID" {
 
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
+}
+
+# general variables
+
+variable "instance_type" {
+ description = "Type of EC2 instance to provision"
+ default     = "t3.nano"
+}
+
+variable "ami_filter" {
+ description = "Name filter and owner for AMI"
+ type = object({
+   name  = string
+   owner = string
+ })
+
+ default = {
+  name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  owner  = "979382823631"
+ }
+}
+
+variable "environment" {
+ description = "Development Environment"
+ type = object({
+    name           = string
+    network_prefix = string
+ })
+
+ default = {
+  name             = "dev"
+  network_prefix   = "10.0"
+ }
+}
+
+variable "asg_min_size" {
+ description = "Minimum number of instances in ASG"
+ default     = "1"
+}
+
+variable "asg_max_size" {
+ description = "Maximum number of instances in ASG"
+ default     = "2"
 }
