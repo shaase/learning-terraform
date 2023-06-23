07967a201f04a1e3e4f26ac40df3aa6f2d21bca1
Stephen Haase <shaase@gmail.com>
2023-06-23 08:14:35 -0600

    testing errors

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "instance_type" {
  description = "Type of EC2 instance to provision"
  default     = "t3.nano"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
