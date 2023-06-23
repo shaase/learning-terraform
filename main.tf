e4bd17d72ee18b10ab3137073f72977628a82c8c
Stephen Haase <shaase@gmail.com>
2023-06-23 10:59:26 -0600

    more fixes

--- main.tf
+++ main.tf
@@ -23,8 +23,6 @@ module "blog_vpc" {
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
