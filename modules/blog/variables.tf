efe945a82cc50e53d7e87faaf19b203a73f8fffb
Stephen Haase <shaase@gmail.com>
2023-06-23 14:22:47 -0600

    variables fixes

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -1,15 +1,3 @@
-# secrets
-
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
-# general variables
-
 variable "instance_type" {
  description = "Type of EC2 instance to provision"
  default     = "t3.nano"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+# secrets
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
