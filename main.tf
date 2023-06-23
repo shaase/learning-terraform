fd50363feed0743523d49d84754c7b4a712aaec6
Stephen Haase <shaase@gmail.com>
2023-06-23 10:25:14 -0600

    create dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,19 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -52,7 +63,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.vpc.public_subnets[0]
   name    = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
